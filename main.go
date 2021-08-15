5e37a9e4f8618881e7c9b19639116f29aec8c8d4
Raiyan Bin Nabi <raiyanbinnabi@gmail.com>
2021-08-16 02:01:03 +0600

    Adding a 404 Page

--- main.go
+++ main.go
@@ -11,6 +11,8 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
 	} else if r.URL.Path == "/contact" {
 		fmt.Fprintf(w, "To get in touch, please send an email <a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a>")
+	} else {
+		fmt.Fprint(w, "The page you were looking for was not found")
 	}
 }
 
