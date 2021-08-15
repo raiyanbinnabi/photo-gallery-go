157b99feeacf4afa20157ea001b9abede81ce8d8
Raiyan Bin Nabi <raiyanbinnabi@gmail.com>
2021-08-15 22:22:21 +0600

    Added a contact page

--- main.go
+++ main.go
@@ -5,10 +5,13 @@ import (
 	"net/http"
 )
 
-
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+	} else if r.URL.Path == "/contact" {
+		fmt.Fprintf(w, "To get in touch, please send an email <a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a>")
+	}
 }
 
 func main() {
